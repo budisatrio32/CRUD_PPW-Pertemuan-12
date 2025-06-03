ffde7e70431d6a4a9c82f5c5fd9542e007e20322
Prihastomo Budi Satrio <prihastomobudisatrio2005@mail.ugm.ac.id>
2025-06-03 23:23:22 +0700

    logout

--- logout.php
+++ logout.php
@@ -1,21 +1,11 @@
 <?php
 session_start();
 
-$username = isset($_SESSION['username']) ? $_SESSION['username'] : 'User';
-
-$_SESSION = array();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
+// Hapus semua session
+session_unset();
 session_destroy();
-echo "<script>
-        alert('Logout berhasil, $username! Terima kasih telah menggunakan ScoreZone.');
-        window.location.href = 'login.php';
-    </script>";
+
+// Redirect ke halaman login
+header("Location: login.php");
 exit();
 ?>
